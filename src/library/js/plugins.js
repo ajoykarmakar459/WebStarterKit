d3d9aa20ff381f575878e65f59cf187308f9754e
ajoy <ajoykarmakar.swadesh@gmail.com>
2020-02-19 18:37:28 +0530

    Plugin JS modified.

--- src/library/js/plugins.js
+++ src/library/js/plugins.js
@@ -1,7 +1,7 @@
 module.exports = {
     JS_Library: [
         './node_modules/jquery/dist/jquery.slim.min.js',
-        "./node_modules/bootstrap/dist/js/bootstrap.bundle.js",
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.js',
         './src/library/js/app.js'
     ]
 };
