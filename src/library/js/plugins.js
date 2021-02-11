b2c2abc371e64d64b5d6e64596e40218a65d2e86
Ajoy Karmakar <ajoykarmakar.swadesh@gmail.com>
2021-02-11 18:32:24 +0530

    Delete plugins.js

--- src/library/js/plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    JS_Library: [
-        "./node_modules/jquery/dist/jquery.js",
-        "./node_modules/bootstrap/dist/js/bootstrap.bundle.js",
-        "./src/library/js/app.js"
-    ]
-};
